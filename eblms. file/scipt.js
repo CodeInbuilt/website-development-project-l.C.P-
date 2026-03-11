ee560755f5b1abe99d7a969e4288cbfc3a5faf43
Rachit Shukla <rachit.shukla2006@gmail.com>
2026-03-11 13:08:47 +0530

    Add files via upload

---
+++ eblms. file/scipt.js
@@ -0,0 +1,356 @@
+index.jsx
+style.css
+import React from "react";
+import group121 from "./group-1-2.png";
+import group1 from "./group-1.png";
+import group2 from "./group-2.png";
+import group3 from "./group-3.png";
+import group4 from "./group-4.png";
+import group5 from "./group-5.png";
+import group6 from "./group-6.png";
+import group7 from "./group-7.png";
+import group8 from "./group-8.png";
+import group9 from "./group-9.png";
+import group10 from "./group-10.png";
+import group11 from "./group-11.png";
+import group12 from "./group-12.png";
+import group from "./group.png";
+import image from "./image.png";
+import image1 from "./image.svg";
+import line4 from "./line-4.svg";
+import maskGroup6 from "./mask-group-6.png";
+import star1 from "./star-1.svg";
+import "./style.css";
+import union from "./union.svg";
+import vector2 from "./vector-2.svg";
+import vector3 from "./vector-3.svg";
+import vector4 from "./vector-4.svg";
+import vector5 from "./vector-5.svg";
+import vector6 from "./vector-6.svg";
+import vector7 from "./vector-7.svg";
+import vector8 from "./vector-8.svg";
+import vector from "./vector.svg";
+
+export const Box = () => {
+  return (
+    <div className="box">
+      <div className="group">
+        <div className="overlap">
+          <div className="desktop">
+            <div className="overlap-group">
+              <div className="div">
+                <div className="group-wrapper">
+                  <div className="overlap-group-wrapper">
+                    <div className="overlap-group-2">
+                      <div className="ellipse" />
+
+                      <img className="union" alt="Union" src={union} />
+                    </div>
+                  </div>
+                </div>
+
+                <div className="text-wrapper">Search.....</div>
+              </div>
+
+              <div className="img-wrapper">
+                <img className="img" alt="Group" src={group1} />
+              </div>
+
+              <div className="div-wrapper">
+                <div className="group-2" />
+              </div>
+
+              <div className="group-3">
+                <div className="group-4">
+                  <div className="group-5">
+                    <img className="vector" alt="Vector" src={vector} />
+
+                    <div className="group-6">
+                      <div className="overlap-group-3">
+                        <img className="vector-2" alt="Vector" src={image1} />
+
+                        <img className="group-7" alt="Group" src={group7} />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="overlap-wrapper">
+                <div className="overlap-2">
+                  <div className="overlap-group-4">
+                    <img className="group-8" alt="Group" src={group10} />
+                  </div>
+
+                  <div className="text-wrapper-2">Event</div>
+                </div>
+              </div>
+            </div>
+
+            <div className="overlap-3">
+              <div className="overlap-4">
+                <div className="rectangle" />
+
+                <div className="text-wrapper-3">Explore</div>
+              </div>
+
+              <div className="ellipse-2" />
+
+              <div className="ellipse-3" />
+
+              <div className="ellipse-4" />
+
+              <div className="ellipse-5" />
+
+              <div className="ellipse-6" />
+
+              <div className="ellipse-7" />
+
+              <div className="ellipse-8" />
+
+              <img className="group-9" alt="Group" src={group} />
+
+              <img className="group-10" alt="Group" src={image} />
+            </div>
+
+            <div className="group-11">
+              <div className="overlap-5">
+                <img className="group-12" alt="Group" src={group2} />
+
+                <img className="group-13" alt="Group" src={group3} />
+              </div>
+            </div>
+
+            <div className="overlap-6">
+              <div className="overlap-7">
+                <div className="group-14">
+                  <div className="group-15">
+                    <div className="overlap-group-5">
+                      <div className="ellipse-9" />
+
+                      <div className="text-wrapper-4">?</div>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="group-16">
+                  <div className="group-17">
+                    <div className="group-18">
+                      <div className="overlap-group-6">
+                        <img className="vector-3" alt="Vector" src={vector2} />
+
+                        <img className="group-19" alt="Group" src={group8} />
+
+                        <img className="group-20" alt="Group" src={group9} />
+
+                        <img className="vector-4" alt="Vector" src={vector6} />
+
+                        <img className="vector-5" alt="Vector" src={vector7} />
+
+                        <img className="vector-6" alt="Vector" src={vector8} />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="group-21">
+                  <div className="overlap-8">
+                    <div className="group-22">
+                      <div className="group-23" />
+                    </div>
+
+                    <div className="group-24">
+                      <div className="overlap-group-7">
+                        <div className="text-wrapper-5">Research</div>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="group-25">
+                    <div className="overlap-9">
+                      <div className="text-wrapper-6">Contact</div>
+                    </div>
+                  </div>
+
+                  <div className="group-26">
+                    <div className="overlap-10">
+                      <div className="text-wrapper-7">FAQ</div>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="group-27" />
+              </div>
+
+              <div className="overlap-11">
+                <div className="group-28">
+                  <div className="group-29">
+                    <div className="group-30">
+                      <div className="group-31">
+                        <div className="overlap-group-8">
+                          <img
+                            className="vector-7"
+                            alt="Vector"
+                            src={vector4}
+                          />
+
+                          <img
+                            className="vector-8"
+                            alt="Vector"
+                            src={vector5}
+                          />
+
+                          <img
+                            className="vector-9"
+                            alt="Vector"
+                            src={vector3}
+                          />
+
+                          <img className="star" alt="Star" src={star1} />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="group-32">
+                  <div className="overlap-12">
+                    <div className="text-wrapper-8">Event</div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="overlap-13">
+                <div className="group-33">
+                  <div className="group-34" />
+                </div>
+
+                <div className="group-35">
+                  <div className="overlap-12">
+                    <div className="text-wrapper-9">Note</div>
+                  </div>
+                </div>
+
+                <div className="group-36" />
+              </div>
+            </div>
+
+            <div className="overlap-14">
+              <div className="overlap-15">
+                <img className="group-37" alt="Group" src={group121} />
+              </div>
+
+              <div className="ellipse-wrapper">
+                <div className="ellipse-10" />
+              </div>
+
+              <div className="overlap-16">
+                <div className="ellipse-10" />
+              </div>
+
+              <div className="overlap-17">
+                <div className="ellipse-10" />
+              </div>
+
+              <div className="overlap-18">
+                <div className="ellipse-10" />
+              </div>
+
+              <div className="group-38">
+                <div className="overlap-19">
+                  <div className="rectangle-2" />
+
+                  <div className="text-wrapper-10">Rachit Shukla</div>
+                </div>
+              </div>
+
+              <div className="group-39">
+                <div className="overlap-2">
+                  <div className="text-wrapper-11">Section -28</div>
+                </div>
+              </div>
+
+              <div className="text-wrapper-12">Attendance</div>
+
+              <div className="group-40">
+                <div className="overlap-20">
+                  <div className="text-wrapper-13">90%</div>
+                </div>
+              </div>
+
+              <div className="text-wrapper-14">:-</div>
+
+              <div className="text-wrapper-15">24SCSECS1310010</div>
+
+              <div className="text-wrapper-16">rachit.shukla@gmail.com</div>
+
+              <div className="text-wrapper-17">B.tech with (G.T)</div>
+            </div>
+
+            <div className="overlap-21">
+              <div className="overlap-22">
+                <div className="text-wrapper-18">Show more list</div>
+
+                <div className="overlap-group-9">
+                  <img className="group-41" alt="Group" src={group5} />
+
+                  <img className="group-42" alt="Group" src={group6} />
+                </div>
+              </div>
+
+              <div className="overlap-23">
+                <img className="group-43" alt="Group" src={group4} />
+
+                <div className="text-wrapper-19">Exam Subject</div>
+              </div>
+
+              <div className="overlap-24">
+                <div className="text-wrapper-20">SEC-28</div>
+              </div>
+
+              <img className="line" alt="Line" src={line4} />
+
+              <div className="text-wrapper-21">TOPIC CLASS</div>
+            </div>
+
+            <div className="overlap-25">
+              <div className="overlap-26">
+                <div className="text-wrapper-22">Teacher</div>
+
+                <div className="group-44" />
+              </div>
+
+              <div className="overlap-27">
+                <div className="group-45" />
+              </div>
+
+              <div className="overlap-28">
+                <div className="text-wrapper-23">course subject</div>
+
+                <div className="group-46" />
+              </div>
+
+              <div className="overlap-29">
+                <div className="rectangle-3" />
+
+                <div className="text-wrapper-24">Book</div>
+
+                <img className="mask-group" alt="Mask group" src={maskGroup6} />
+              </div>
+
+              <div className="group-47">
+                <div className="overlap-30">
+                  <img className="group-48" alt="Group" src={group11} />
+
+                  <img className="group-49" alt="Group" src={group12} />
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="text-wrapper-25">Class Member</div>
+        </div>
+      </div>
+    </div>
+  );
+}
